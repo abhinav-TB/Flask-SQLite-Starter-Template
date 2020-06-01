1063b99dec530269ffa6cff094eeab3c0ecc5118
abhinav-TB <abhinavtb@gmail.com>
2020-06-01 18:33:17 +0530

    installed  pakages

--- app.py
+++ app.py
@@ -17,50 +17,19 @@ class Todo(db.Model):
 
 @app.route('/',methods=['POST','GET'])
 def index():
+    pass
 
-    if request.method=='POST':
-        task_content= request.form['content']
-        new_task =Todo(content=task_content)
-        
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return"error"
-    else:
-        task=Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html',tasks=task)
+   
+    
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    task_to_delete=Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    
-    except:
-        return 'There was a problem deletng that task'
+    pass
 
 @app.route('/update/<int:id>',methods=['GET','POST'])
 
 def update(id):
-    task =Todo.query.get_or_404(id)
-
-    if request.method=="POST":
-        task.content=request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-
-        except:
-            return "there was an issue updating your task"
-    
-    else:
-        return render_template('update.html',task=task)
+   pass
 
 if __name__=="__main__":
     app.run(debug=True)
